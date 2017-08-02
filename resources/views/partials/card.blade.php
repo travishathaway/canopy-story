a2b531d91105397a7ec8b3b3cef00494f5cab0b5
Travis Hathaway <travis.j.hathaway@gmail.com>
2017-08-01 20:49:02 -0700

    fixing some more layout issues

--- resources/views/partials/card.blade.php
+++ resources/views/partials/card.blade.php
@@ -33,78 +33,82 @@
       </div>
     </div>
     @endif
-    <div class="row">
-      <div class="col-md-9">
-        <strong>By: </strong>{{ $post->user->first_name}}  {{ strtoupper(substr($post->user->last_name, 0, 1)) . '.'}}
-      </div>
-      <div class="col-md-3">
-        @if($user) 
-        @if($user->id == $post->user_id || $user->isAdmin())
-        <form method="POST" onsubmit="return confirm('Are you sure you want to delete this post?')" action="{{route('post.delete', $post->id)}}"
-          class="pull-right">
-          {{ csrf_field() }}
-          <input type="hidden" value="8" name="id">
-          <button class="style-as-link pull-right" type="submit" title="Flag as Inappropriate?">
-            <span class="glyphicon glyphicon-trash"></span>
-          </button>
-        </form>
-        @endif
-        @if($user->isAdmin())
-        <form method="POST" onsubmit="return confirm('Are you sure you want to flag this post?')" action="{{route('post.update', $post->id)}}"
-          class="pull-right" style="margin-right: 3px">
-          {{ csrf_field() }}
-          <input type="hidden" value="1" name="flagged">
-          <button class="style-as-link pull-right" type="submit" title="Flag as Inappropriate?">
-            <span class="glyphicon glyphicon-flag"></span>
-          </button>
-        </form>
-        @endif
-        @endif
-      </div>
-    </div>
-    <div class="row">
+    <div class="card-body row">
       <div class="col-md-12">
-        <strong>Location: </strong>
-        <a class="" href="{{ url('post') }}?q={{ $post->tree_location }}">
-          {{ $post->tree_location }}
-        </a>
-      </div>
-    </div>
-    <br />
-    <div class="row">
-      <div class="col-md-12">
-        <blockquote class="">
-        {{ str_limit($post->content, 250) }}
-        @if(strlen($post->content) > 250)
-        <div class="text-right">
-          <a href="#" title="Read More" data-toggle="modal" data-target="#post-detail-modal-{{$loop->iteration}}">
-            Read More...
-          </a>
-        </div>
-        <div class="modal fade" id="post-detail-modal-{{$loop->iteration}}" tabindex="-1" 
-            role="dialog" aria-labelledby="postModalDetail">
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                  Canopy story at <strong>{{ $post->tree_location }}</strong> by
-                  <strong>
-                    {{ $post->user->first_name}} {{ strtoupper(substr($post->user->last_name, 0, 1)) . '.'}}
-                  </strong>
-              </div>
-              <div class="modal-body">
-                {!! nl2br(e($post->content)) !!}
-              </div>
-            </div>
+        <div class="row">
+          <div class="col-md-9">
+            <strong>By: </strong>{{ $post->user->first_name}}  {{ strtoupper(substr($post->user->last_name, 0, 1)) . '.'}}
+          </div>
+          <div class="col-md-3">
+            @if($user) 
+            @if($user->id == $post->user_id || $user->isAdmin())
+            <form method="POST" onsubmit="return confirm('Are you sure you want to delete this post?')" action="{{route('post.delete', $post->id)}}"
+              class="pull-right">
+              {{ csrf_field() }}
+              <input type="hidden" value="8" name="id">
+              <button class="style-as-link pull-right" type="submit" title="Flag as Inappropriate?">
+                <span class="glyphicon glyphicon-trash"></span>
+              </button>
+            </form>
+            @endif
+            @if($user->isAdmin())
+            <form method="POST" onsubmit="return confirm('Are you sure you want to flag this post?')" action="{{route('post.update', $post->id)}}"
+              class="pull-right" style="margin-right: 3px">
+              {{ csrf_field() }}
+              <input type="hidden" value="1" name="flagged">
+              <button class="style-as-link pull-right" type="submit" title="Flag as Inappropriate?">
+                <span class="glyphicon glyphicon-flag"></span>
+              </button>
+            </form>
+            @endif
+            @endif
           </div>
         </div>
-        @endif
-        </blockquote>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-12">
-        <strong>Posted: </strong>{{ $post->created_at->diffForHumans()  }}
+        <div class="row">
+          <div class="col-md-12">
+            <strong>Location: </strong>
+            <a class="" href="{{ url('post') }}?q={{ $post->tree_location }}">
+              {{ $post->tree_location }}
+            </a>
+          </div>
+        </div>
+        <br />
+        <div class="row">
+          <div class="col-md-12">
+            <blockquote class="">
+            {{ str_limit($post->content, 250) }}
+            @if(strlen($post->content) > 250)
+            <div class="text-right">
+              <a href="#" title="Read More" data-toggle="modal" data-target="#post-detail-modal-{{$loop->iteration}}">
+                Read More...
+              </a>
+            </div>
+            <div class="modal fade" id="post-detail-modal-{{$loop->iteration}}" tabindex="-1" 
+                role="dialog" aria-labelledby="postModalDetail">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                      Canopy story at <strong>{{ $post->tree_location }}</strong> by
+                      <strong>
+                        {{ $post->user->first_name}} {{ strtoupper(substr($post->user->last_name, 0, 1)) . '.'}}
+                      </strong>
+                  </div>
+                  <div class="modal-body">
+                    {!! nl2br(e($post->content)) !!}
+                  </div>
+                </div>
+              </div>
+            </div>
+            @endif
+            </blockquote>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <strong>Posted: </strong>{{ $post->created_at->diffForHumans()  }}
+          </div>
+        </div>
       </div>
     </div>
   </div>
--- resources/views/posts/list.blade.php
+++ resources/views/posts/list.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid">
-    <div class="col-md-12 white-bkgrnd search-header">
-        <div class="pull-right" id="profile-buttons">
+<div class="container">
+  <div class="row">
+    <div class="col-sm-12 white-bkgrnd search-header">
+      <div class="pull-right" id="profile-buttons">
         @if(\App::getLocale() === 'en')
         <a href="?lang=es" class="btn btn-default btn-sm">
           <span class="text-muted"><span class="lang-sm lang-lbl" lang="es"></span></span>
@@ -18,45 +19,88 @@
         @else
         <a href="/login" class="btn btn-sm btn-default pull-right">Login</a>
         @endif
-        </div>
-        <div class="col-md-12 text-center"><h1><a id="title-link" href="/">{{ config('app.name') }}</a></h1><p/>
-        <h4>What&rsquo;s new in your neck of the woods?</h4></div>
-        <form action="" method="get">
-            <div class="row">
-                <div class="col-md-offset-1 col-md-8">
-                    <input type="text" class="form-control" name="q" placeholder="Seach for a Tree Story"
-                        value="{{ $q }}"/>
-                </div>
-                <div class="col-md-3">
-                    <div class="btn-group pull-right" id="search-buttons">
-                        <button type="submit" class="btn btn-default">Search</button>
-                        <a href="{{ url('post') }}" class='btn btn-warning'>Clear</a>
-                        <a href="{{ url('/') }}" class="btn btn-default" id="list_back_button">Back</a>
-                    </div>
-                </div>
+      </div>
+      <div class="col-md-12 text-center">
+        <h1><a id="title-link" href="/">{{ config('app.name') }}</a></h1>
+        <h4>What&rsquo;s new in your neck of the woods?</h4>
+      </div>
+      <form action="" method="get">
+        <div class="row">
+          <div class="col-md-offset-1 col-md-8">
+            <input type="text" class="form-control" name="q" placeholder="Seach for a Tree Story"
+                value="{{ $q }}"/>
+          </div>
+          <div class="col-md-3">
+            <div class="btn-group pull-right" id="search-buttons">
+                <button type="submit" class="btn btn-default">Search</button>
+                <a href="{{ url('post') }}" class='btn btn-warning'>Clear</a>
+                <a href="{{ url('/') }}" class="btn btn-default" id="list_back_button">Back</a>
             </div>
-        </form>
+          </div>
+        </div>
+      </form>
     </div>
-</div>
-<br />
-@if($posts->count() > 0)
-<div class="container-fluid">
-      @foreach ($posts as $post)
-        @include('partials.card')
-      @endforeach
+  </div>
+  <br />
+  @if($posts->count() > 0)
+  <div class="visible-lg visible-md">
+    <div class="row">
+    @foreach ($posts as $post)
+      @include('partials.card')
+      @if($loop->iteration % 3 == 0)
+      </div>
+        @if(!$loop->last)
+        <div class="row">
+        @endif
+      @endif
+      @if($loop->last)
+      </div>
+      @endif
+    @endforeach
+  </div>
+  <div class="visible-sm">
+    <div class="row">
+    @foreach ($posts as $post)
+      @include('partials.card')
+      @if($loop->iteration % 2 == 0)
+      </div>
+        @if(!$loop->last)
+        <div class="row">
+        @endif
+      @endif
+      @if($loop->last)
+      </div>
+      @endif
+    @endforeach
+  </div>
+  <div class="visible-xs">
+    <div class="row">
+    @foreach ($posts as $post)
+      @include('partials.card')
+      @if($loop->iteration % 1 == 0)
+      </div>
+      @if(!$loop->last)
+      <div class="row">
+      @endif
+        @if($loop->last)
+        </div>
+        @endif
+      @endif
+    @endforeach
   </div>
   <div class="text-center">
     {{ $posts->appends(['q' => $q])->links() }}
   </div>
-</div>
-@else
-<div class="container">
-    <div class="text-center">
-      <h3>No Results</h3>
-      <p>Please try searching for other terms</p>
-      <br />
+  @else
+  <div class="row">
+    <div class="col-md-12 white-bkgrnd">
+      <div class="text-center">
+        <h3>No Results</h3>
+        <p>Please try searching for other terms</p>
+        <br />
+      </div>
     </div>
-</div>
+  </div>
 @endif
 @endsection
 
