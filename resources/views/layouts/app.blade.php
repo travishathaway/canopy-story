a12e79ec2ce0985dc6f2975a90daf95632a3b263
Travis Hathaway <travis.j.hathaway@gmail.com>
2017-09-10 15:55:14 -0700

    bugfix: finally adding GA to the site for use in production mode, fixing bugging modal layout for long stories

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,19 @@
     @section('style')
     @show
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    @if(App::environment() == 'prod')
+      <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  
+        ga('create', 'UA-97391921-3', 'auto');
+        ga('send', 'pageview');
+  
+      </script>
+    @endif
   </head>
   <body>
     @include('partials.site-menu')
