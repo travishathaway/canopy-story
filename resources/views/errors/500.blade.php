c1749c17e64f102e981722b897894953669774e8
Travis Hathaway <travis.j.hathaway@gmail.com>
2017-12-23 16:45:32 -0800

    adding email in 500 page that isn't from config file

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -4,7 +4,7 @@
   <div class="row">
     <div class="ml-auto col-sm-12 col-lg-8 mr-auto">
       <h1 class="mt-4">Server Error</h1>
-      Please contact us <a href="mailto:{{config('share_email')}}" title="Report error">here</a>
+      Please contact us <a href="mailto:canopystory@pdx.edu" title="Report error">here</a>
       if this problem persist.
     </div>
   </div>
